aa6b340951b01d1cae2b6beb5e5b3ff90cb486a0
Xavier Solé <xavisn1@gmail.com>
2018-03-15 22:53:52 +0100

    Unity: Add VideoControl Script.

---
+++ 3DVideoViewer/Assets/Scripts/VideoControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoControl : MonoBehaviour {
+
+	private UnityEngine.Video.VideoPlayer videoPlayer;
+
+
+	[SerializeField]
+	private AudioSource audioSource;
+
+
+
+	void Start () {
+		videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer> ();
+
+
+		if (videoPlayer.clip != null) 
+		{
+			videoPlayer.EnableAudioTrack (0, true);
+			videoPlayer.SetTargetAudioSource(0, audioSource);
+		}
+	}
+
+	//Check if input keys have been pressed and call methods accordingly.
+	void Update(){
+		//Play or pause the video.
+		if (Input.GetKeyDown ("space")) 
+		{
+			if (videoPlayer.isPlaying)
+				videoPlayer.Pause ();
+			else
+				videoPlayer.Play();
+			audioSource.Play();
+		}
+
+	}
+}
